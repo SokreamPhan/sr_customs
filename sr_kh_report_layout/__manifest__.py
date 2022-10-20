ee01909b49eb54eb20f326f867ec2b85de6d1af0
Sokrem Phan <sok.ream06@gmail.com>
2022-10-20 20:12:18 +0700

    [ADD] sr_kh_report_layout

---
+++ sr_kh_report_layout/__manifest__.py
@@ -0,0 +1,20 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+{
+    "name": "Cambodia Localization - Tax Invoice & Commerical Invoice",
+    "summary": """
+            This addon will add two new print options on Customer Invoices,
+            following Cambodia's Tax Invoice and Commercial Invoice
+        """,
+    "version": "15.0.1.0.0",
+    "author": "SokreamPhan",
+    "license": "AGPL-3",
+    "website": "",
+    "category": "Localization / Accounting",
+    "depends": ["account"],
+    "data": [
+        "views/res_partner_views.xml",
+        "views/report_kh_tax_invoice.xml",
+        "views/report_kh_commercial_invoice.xml",
+    ],
+    "installable": True,
+}
